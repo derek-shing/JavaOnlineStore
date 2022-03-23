6b2ffb23af7c8becbca933cf99d98f01b3b7a94a
derek.shing <hpshing@gmail.com>
2022-03-22 21:43:12 -0700

    First Commit

---
+++ onelinestoredemo/src/main/java/derek/onelinestoredemo/OnelinestoredemoApplication.java
@@ -0,0 +1,13 @@
+package derek.onelinestoredemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OnelinestoredemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OnelinestoredemoApplication.class, args);
+	}
+
+}
